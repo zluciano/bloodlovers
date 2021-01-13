4947a4944cf8a979cad31f0b7a845d09cd0fa788
José Luciano <joseluciano@BUS-NB-5ZFMD93.local>
2021-01-13 18:08:41 -0300

    Created models for person and resources

--- bloodsite/blood/models.py
+++ bloodsite/blood/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Doner(models.Model):
+    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blood_type = models.CharField(max_length=3)
+    birth_date = models.DateTimeField(blank=True, null=True)
+    cpf = models.CharField(max_length=11)
+    donating_date = models.DateTimeField(default=timezone.now)
+
+
+class Resource(models.Model):
+    type = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    supply = models.IntegerField()
